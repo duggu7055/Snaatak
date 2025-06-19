d5dc621d86a703cb2287b7e33d4161cc97cbca2c
Durgesh Sharma <111552304+duggu7055@users.noreply.github.com>
2025-06-19 12:18:26 +0530

    Update RunSonarQubeAnalysis.groovy

--- sprint4/src/org/downtimecrew/reactSCA/RunSonarQubeAnalysis.groovy
+++ sprint4/src/org/downtimecrew/reactSCA/RunSonarQubeAnalysis.groovy
@@ -1,1 +1,16 @@
+package org.downtimecrew.reactSCA
+
+def call(String projectKey, String sonarUrl, String sonarSources, String sonarToken) {
+    stage('Run SonarQube Analysis') {
+        withCredentials([string(credentialsId: sonarToken, variable: 'SONARQUBE_AUTH_TOKEN')]) {
+            sh """
+                /opt/sonar-scanner/bin/sonar-scanner \
+                -Dsonar.projectKey=${projectKey} \
+                -Dsonar.sources=${sonarSources} \
+                -Dsonar.host.url=${sonarUrl} \
+                -Dsonar.login=${env.SONARQUBE_AUTH_TOKEN}
+            """
+        }
+    }
+}
 
