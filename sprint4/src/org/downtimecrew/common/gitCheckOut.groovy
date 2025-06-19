c745d536df53b9dfabd0bb88fc099659cbca7230
Durgesh Sharma <111552304+duggu7055@users.noreply.github.com>
2025-06-19 12:17:39 +0530

    Update gitCheckOut.groovy

--- sprint4/src/org/downtimecrew/common/gitCheckOut.groovy
+++ sprint4/src/org/downtimecrew/common/gitCheckOut.groovy
@@ -1,1 +1,8 @@
+package org.downtimecrew.common
+
+def call(String branch, String url, String creds) {
+    stage('Checkout Code') {
+        git branch: branch, url: url, credentialsId: creds
+    }
+}
 
