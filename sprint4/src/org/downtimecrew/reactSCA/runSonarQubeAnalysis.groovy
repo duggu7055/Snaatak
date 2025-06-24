5cd6d47b234b1285bc6ce67076803d876c7c2baa
Durgesh Sharma <111552304+duggu7055@users.noreply.github.com>
2025-06-24 13:25:00 +0530

    Update runSonarQubeAnalysis.groovy

--- sprint4/src/org/downtimecrew/reactSCA/runSonarQubeAnalysis.groovy
+++ sprint4/src/org/downtimecrew/reactSCA/runSonarQubeAnalysis.groovy
@@ -1,16 +1,16 @@
 package org.downtimecrew.reactSCA
 
-def call(String projectKey, String sonarUrl, String sonarSources, String sonarToken) {
-    stage('Run SonarQube Analysis') {
-        withCredentials([string(credentialsId: sonarToken, variable: 'SONARQUBE_AUTH_TOKEN')]) {
+def call(Map config = [:]) {
+    stage(config.stageName ?: 'Run SonarQube Analysis') {
+        withCredentials([string(credentialsId: config.sonarCredentialId ?: env.SONAR_CRED_ID ?: 'default-sonar-cred', variable: 'SONARQUBE_AUTH_TOKEN')]) {
             sh """
-                /opt/sonar-scanner/bin/sonar-scanner \
-                -Dsonar.projectKey=${projectKey} \
-                -Dsonar.sources=${sonarSources} \
-                -Dsonar.host.url=${sonarUrl} \
-                -Dsonar.login=${env.SONARQUBE_AUTH_TOKEN}
+                ${config.sonarScannerPath ?: env.SONAR_SCANNER_PATH ?: '/opt/sonar-scanner/bin/sonar-scanner'} \
+                -Dsonar.projectKey=${config.projectKey ?: env.SONAR_PROJECT_KEY ?: 'default-project-key'} \
+                -Dsonar.sources=${config.sonarSources ?: env.SONAR_SOURCES ?: 'src'} \
+                -Dsonar.javascript.lcov.reportPaths=${config.lcovPath ?: env.LCOV_PATH ?: 'coverage/lcov.info'} \
+                -Dsonar.login=$SONARQUBE_AUTH_TOKEN \
+                -Dsonar.host.url=${config.sonarUrl ?: env.SONAR_HOST_URL ?: 'http://localhost:9000'}
             """
         }
     }
 }
-
