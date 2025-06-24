c423d86b0742da273ebf78d39d03c8822c2ce915
Durgesh Sharma <111552304+duggu7055@users.noreply.github.com>
2025-06-24 13:25:21 +0530

    Update reactSCA.groovy

--- sprint4/src/org/downtimecrew/template/reactSCA/reactSCA.groovy
+++ sprint4/src/org/downtimecrew/template/reactSCA/reactSCA.groovy
@@ -3,15 +3,16 @@ package org.downtimecrew.template.reactSCA
 import org.downtimecrew.common.*
 import org.downtimecrew.reactSCA.*
 
-def call(String branch, String url, String creds, String projectKey, String sonarUrl, String sonarSources, String sonarToken) {
+def call(Map config = [:]) {
     def wsClean = new wsClean()
-    def gitCheckOut = new gitCheckOut()    
+    def gitCheckOut = new gitCheckOut()
     def installDependencies = new installDependencies()
+    def auditFix = new auditFix()
     def runSonarQubeAnalysis = new runSonarQubeAnalysis()
 
-    wsClean.call()
-    gitCheckOut.call(branch, url, creds)
-    installDependencies.call()
-    runSonarQubeAnalysis.call(projectKey, sonarUrl, sonarSources, sonarToken)
+    wsClean.call(config.cleanWorkspace ?: [:])
+    gitCheckOut.call(config.gitCheckout ?: [:])
+    installDependencies.call(config.installDeps ?: [:])
+    auditFix.call(config.auditFix ?: [:])
+    runSonarQubeAnalysis.call(config.sonarQube ?: [:])
 }
-
