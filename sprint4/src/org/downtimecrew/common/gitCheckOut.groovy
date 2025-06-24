39e0be6539f0934cdb70886e0691dbac92a98d06
Durgesh Sharma <111552304+duggu7055@users.noreply.github.com>
2025-06-24 13:23:37 +0530

    Update gitCheckOut.groovy

--- sprint4/src/org/downtimecrew/common/gitCheckOut.groovy
+++ sprint4/src/org/downtimecrew/common/gitCheckOut.groovy
@@ -1,8 +1,14 @@
 package org.downtimecrew.common
 
-def call(String branch, String url, String creds) {
-    stage('Checkout Code') {
-        git branch: branch, url: url, credentialsId: creds
+def call(Map config = [:]) {
+    stage(config.stageName ?: 'Checkout Code') {
+        checkout([
+            $class: 'GitSCM',
+            branches: [[name: config.branch ?: env.GIT_BRANCH ?: 'main']],
+            userRemoteConfigs: [[
+                url: config.repoUrl ?: env.GIT_REPO_URL ?: 'https://github.com/OT-MICROSERVICES/frontend.git',
+                credentialsId: config.creds ?: env.GIT_CREDS_ID ?: 'default-credentials-id'
+            ]]
+        ])
     }
 }
-
