0a6e262a1b4e94d9f09e0e33fb1ab5bf6801ba70
Durgesh Sharma <111552304+duggu7055@users.noreply.github.com>
2025-07-10 01:20:38 +0530

    Update terraformFmt.groovy

--- sprint4/src/org/downtimecrew/terraform/terraformFmt.groovy
+++ sprint4/src/org/downtimecrew/terraform/terraformFmt.groovy
@@ -1,12 +1,14 @@
 package org.downtimecrew.terraform
 
-def call(String Directory) {
-    stage("Terraform Fmt") {
-        script {
-            sh """
-                cd ${Directory}
-                terraform fmt
-            """
+class terraformFmt {
+    def call(String Directory) {
+        stage("Terraform Fmt") {
+            script {
+                sh """
+                    cd ${Directory}
+                    terraform fmt
+                """
+            }
         }
     }
 }
