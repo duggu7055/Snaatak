ccbf571e8da9523560fdac19ebee275323beeb60
Durgesh Sharma <111552304+duggu7055@users.noreply.github.com>
2025-07-09 23:16:06 +0530

    Create terraform.groovy

---
+++ sprint4/src/org/downtimecrew/template/terraformCD/terraform.groovy
@@ -0,0 +1,26 @@
+package org.downtimecrew.template.terraformCD
+import org.downtimecrew.common.*
+import org.downtimecrew.reactSCA.*
+
+def call(String url, String branch, String creds, String Directory) {
+
+    // Create instances of required classes
+     def wsClean=new wsClean()
+     def gitCheckOut = new gitCheckOut()
+     def terraformInit = new terraformInit()
+     def terraformFmt = new terraformFmt()
+     def terraformValidate = new terraformValidate()
+     def terraformPlan = new terraformPlan()
+     def terraformapply = new terraformapply()
+
+
+    // Call methods with correct arguments
+    wsClean.call()
+    gitCheckOut.call(url, branch, creds)
+    terraformInit.call(Directory)
+    terraformFmt.call(Directory)
+    terraformValidate.call(Directory)
+    terraformPlan.call(Directory)
+    terraformapply.call(Directory)
+
+}
