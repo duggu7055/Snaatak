87a931e0af8d28037e042942a946116ca9b5ea2a
Durgesh Sharma <111552304+duggu7055@users.noreply.github.com>
2025-07-10 01:22:02 +0530

    Update terraformapply.groovy

--- sprint4/src/org/downtimecrew/terraform/terraformapply.groovy
+++ sprint4/src/org/downtimecrew/terraform/terraformapply.groovy
@@ -1,34 +1,36 @@
 package org.downtimecrew.terraform
 
-def call(String Directory) {
-    stage("Terraform Action") {
-        script {
-            def actionChoice = input(
-                message: "Which Terraform action would you like to perform?",
-                parameters: [
-                    choice(name: "ACTION", choices: ["apply", "destroy", "skip"], description: "Choose the Terraform action")
-                ]
-            )
+class terraformapply {
+    def call(String Directory) {
+        stage("Terraform Action") {
+            script {
+                def actionChoice = input(
+                    message: "Which Terraform action would you like to perform?",
+                    parameters: [
+                        choice(name: "ACTION", choices: ["apply", "destroy", "skip"], description: "Choose the Terraform action")
+                    ]
+                )
 
-            if (actionChoice == "skip") {
-                echo "Terraform action skipped by the user."
-                return
-            }
+                if (actionChoice == "skip") {
+                    echo "Terraform action skipped by the user."
+                    return
+                }
 
-            def userApproval = input(
-                message: "Do you want to proceed with Terraform ${actionChoice}?",
-                parameters: [
-                    choice(name: "CONFIRM", choices: ["Yes", "No"], description: "Confirm your action")
-                ]
-            )
+                def userApproval = input(
+                    message: "Do you want to proceed with Terraform ${actionChoice}?",
+                    parameters: [
+                        choice(name: "CONFIRM", choices: ["Yes", "No"], description: "Confirm your action")
+                    ]
+                )
 
-            if (userApproval == "Yes") {
-                sh """
-                    cd ${Directory}
-                    terraform ${actionChoice} -auto-approve
-                """
-            } else {
-                echo "Terraform ${actionChoice} was skipped by the user."
+                if (userApproval == "Yes") {
+                    sh """
+                        cd ${Directory}
+                        terraform ${actionChoice} -auto-approve
+                    """
+                } else {
+                    echo "Terraform ${actionChoice} was skipped by the user."
+                }
             }
         }
     }
