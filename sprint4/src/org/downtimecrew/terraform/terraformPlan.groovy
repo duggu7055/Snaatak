d65fe958d07b8f833cc036d517febf3a79dca2c8
Durgesh Sharma <111552304+duggu7055@users.noreply.github.com>
2025-07-10 01:21:46 +0530

    Update terraformPlan.groovy

--- sprint4/src/org/downtimecrew/terraform/terraformPlan.groovy
+++ sprint4/src/org/downtimecrew/terraform/terraformPlan.groovy
@@ -1,12 +1,14 @@
 package org.downtimecrew.terraform
 
-def call(String Directory) {
-    stage("Terraform plan") {
-        script {
-            sh """
-                cd ${Directory}
-                terraform plan
-            """
+class terraformPlan {
+    def call(String Directory) {
+        stage("Terraform plan") {
+            script {
+                sh """
+                    cd ${Directory}
+                    terraform plan
+                """
+            }
         }
     }
 }
