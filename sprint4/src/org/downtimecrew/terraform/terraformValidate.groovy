20d1c230808931afa7e837ccb092f06b9cbdc6e6
Durgesh Sharma <111552304+duggu7055@users.noreply.github.com>
2025-07-10 01:21:30 +0530

    Update terraformValidate.groovy

--- sprint4/src/org/downtimecrew/terraform/terraformValidate.groovy
+++ sprint4/src/org/downtimecrew/terraform/terraformValidate.groovy
@@ -1,12 +1,14 @@
 package org.downtimecrew.terraform
 
-def call(String Directory) {
-    stage("Terraform validate") {
-        script {
-            sh """
-                cd ${Directory}
-                terraform validate
-            """
+class terraformValidate {
+    def call(String Directory) {
+        stage("Terraform validate") {
+            script {
+                sh """
+                    cd ${Directory}
+                    terraform validate
+                """
+            }
         }
     }
 }
