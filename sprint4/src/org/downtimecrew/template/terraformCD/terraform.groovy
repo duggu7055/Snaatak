76b6ade4d2c2440c271851af7d3f90fb17bc5064
Durgesh Sharma <111552304+duggu7055@users.noreply.github.com>
2025-07-10 00:59:23 +0530

    Update terraform.groovy

--- sprint4/src/org/downtimecrew/template/terraformCD/terraform.groovy
+++ sprint4/src/org/downtimecrew/template/terraformCD/terraform.groovy
@@ -1,26 +1,24 @@
 package org.downtimecrew.template.terraformCD
+
 import org.downtimecrew.common.*
 import org.downtimecrew.terraform.*
 
-def call(String url, String branch, String creds, String Directory) {
+class terraform {
+    def call(String url, String branch, String creds, String Directory) {
+        def wsClean = new wsClean()
+        def gitCheckOut = new gitCheckOut()
+        def terraformInit = new terraformInit()
+        def terraformFmt = new terraformFmt()
+        def terraformValidate = new terraformValidate()
+        def terraformPlan = new terraformPlan()
+        def terraformapply = new terraformapply()
 
-    // Create instances of required classes
-     def wsClean=new wsClean()
-     def gitCheckOut = new gitCheckOut()
-     def terraformInit = new terraformInit()
-     def terraformFmt = new terraformFmt()
-     def terraformValidate = new terraformValidate()
-     def terraformPlan = new terraformPlan()
-     def terraformapply = new terraformapply()
-
-
-    // Call methods with correct arguments
-    wsClean.call()
-    gitCheckOut.call(url, branch, creds)
-    terraformInit.call(Directory)
-    terraformFmt.call(Directory)
-    terraformValidate.call(Directory)
-    terraformPlan.call(Directory)
-    terraformapply.call(Directory)
-
+        wsClean.call()
+        gitCheckOut.call(url, branch, creds)
+        terraformInit.call(Directory)
+        terraformFmt.call(Directory)
+        terraformValidate.call(Directory)
+        terraformPlan.call(Directory)
+        terraformapply.call(Directory)
+    }
 }
