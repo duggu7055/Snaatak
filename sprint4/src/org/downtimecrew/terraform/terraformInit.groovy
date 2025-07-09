ddb3a2013612abba3b6719eace20124e78541c2f
Durgesh Sharma <111552304+duggu7055@users.noreply.github.com>
2025-07-10 01:20:13 +0530

    Update terraformInit.groovy

--- sprint4/src/org/downtimecrew/terraform/terraformInit.groovy
+++ sprint4/src/org/downtimecrew/terraform/terraformInit.groovy
@@ -1,12 +1,14 @@
 package org.downtimecrew.terraform
 
-def call(String Directory) {
-    stage("Terraform init") {
-        script {
-            sh """
-                cd ${Directory}
-                terraform init -reconfigure
-            """
+class terraformInit {
+    def call(String Directory) {
+        stage("Terraform init") {
+            script {
+                sh """
+                    cd ${Directory}
+                    terraform init -reconfigure
+                """
+            }
         }
     }
 }
