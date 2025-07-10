10e610d74bcc1a4097c8b2f28934ffa48830081f
Durgesh Sharma <111552304+duggu7055@users.noreply.github.com>
2025-07-11 02:33:10 +0530

    Update terraformInit.groovy

--- src/org/downtimecrew/terraform/terraformInit.groovy
+++ src/org/downtimecrew/terraform/terraformInit.groovy
@@ -1,13 +1,29 @@
 package org.downtimecrew.terraform
 
-class terraformInit {
-    def call(String Directory) {
-        stage("Terraform init") {
-            script {
-                sh """
-                    cd ${Directory}
-                    terraform init -reconfigure
-                """
+def run(Map config) {
+    stage('Terraform Init') {
+        def tfDir = config?.terraformDir?.trim()
+        def awsCredId = config?.awsCredId?.trim()
+
+        if (!tfDir) {
+            error "Terraform Init: 'terraformDir' is missing or empty in config map"
+        }
+        if (!awsCredId) {
+            error "Terraform Init: 'aws_cred_id' is missing or empty in config map"
+        }
+
+        echo "Initializing Terraform in: ${tfDir}"
+        sh "ls -la ${tfDir}"
+
+        withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: awsCredId]]) {
+            dir(tfDir) {
+                def initCommand = 'terraform init'
+                if (config.reconfigure) {
+                    initCommand += ' -reconfigure'
+                }
+
+                echo "Running: ${initCommand}"
+                sh initCommand
             }
         }
     }
