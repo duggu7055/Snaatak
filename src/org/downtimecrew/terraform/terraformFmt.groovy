a459d202c23fff98ba5842eb077243acb366bb43
Durgesh Sharma <111552304+duggu7055@users.noreply.github.com>
2025-07-11 02:32:43 +0530

    Update terraformFmt.groovy

--- src/org/downtimecrew/terraform/terraformFmt.groovy
+++ src/org/downtimecrew/terraform/terraformFmt.groovy
@@ -1,14 +1,17 @@
 package org.downtimecrew.terraform
 
-class terraformFmt {
-    def call(String Directory) {
-        stage("Terraform Fmt") {
-            script {
-                sh """
-                    cd ${Directory}
-                    terraform fmt
-                """
-            }
+def run(Map config) {
+    stage('Terraform Format') {
+        if (!config?.terraformDir) {
+            error "Terraform Format: 'terraformDir' value is null or empty in config map"
+        }
+
+        echo "TF_WORKING_DIR: ${config.terraformDir}"
+        sh "ls -la ${config.terraformDir}"
+
+        dir(config.terraformDir) {
+            sh 'terraform fmt'
         }
     }
 }
+
