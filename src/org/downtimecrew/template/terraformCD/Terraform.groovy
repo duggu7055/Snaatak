be402e8e1221f1b0b52858bcb6903c84a7ad1362
Durgesh Sharma <111552304+duggu7055@users.noreply.github.com>
2025-07-11 02:38:41 +0530

    Update Terraform.groovy

--- src/org/downtimecrew/template/terraformCD/Terraform.groovy
+++ src/org/downtimecrew/template/terraformCD/Terraform.groovy
@@ -3,28 +3,54 @@ package org.downtimecrew.template.terraformCD
 import org.downtimecrew.common.*
 import org.downtimecrew.terraform.*
 
-class Terraform {
-    def call(String url, String branch, String creds, String Directory) {
-        def wsClean = new wsClean()
-        def gitCheckOut = new gitCheckOut()
-        def terraformInit = new terraformInit()
-        def terraformFmt = new terraformFmt()
-        def terraformValidate = new terraformValidate()
-        def terraformPlan = new terraformPlan()
-        def terraformapply = new terraformapply()
+    def call(Map config = [:]) {
+    def wsClean = new cleanWorkspace()
+    def gitCheckOut = new checkout()
+    def fmt = new fmt()
+    def init = new init()
+    def validate = new validate()
+    def plan = new plan()
+    def apply = new apply()
 
+    try {
         wsClean.call()
-
         gitCheckOut.call([
-            repoUrl: url,
-            branch: branch,
-            creds: creds
+            branch  : config.gitBranch,
+            repoUrl : config.gitRepoUrl,
+            creds   : config.gitCredsId
         ])
+        init.run(config)
+        fmt.run(config)
+        validate.run(config)
+        plan.run(config)
 
-        terraformInit.call(Directory)
-        terraformFmt.call(Directory)
-        terraformValidate.call(Directory)
-        terraformPlan.call(Directory)
-        terraformapply.call(Directory)
+        def reportLinks = [
+            [name: 'Terraform Plan Output', url: "${env.BUILD_URL}artifact/terraform-plan.txt"]
+        ]
+
+        notify.sendNotification([
+            status      : 'SUCCESS',
+            slackChannel: config.slackChannel,
+            slackCredId : config.slackCredId,
+            emailTo     : config.emailTo,
+            reportLinks : reportLinks
+        ], this)
+
+    } catch (Exception e) {
+        def reportLinks = [
+            [name: 'Terraform Plan Output', url: "${env.BUILD_URL}artifact/terraform-plan.txt"]
+        ]
+
+        notify.sendNotification([
+            status       : 'FAILURE',
+            failureReason: e.message,
+            failedStage  : env.STAGE_NAME,
+            slackChannel : config.slackChannel,
+            slackCredId  : config.slackCredId,
+            emailTo      : config.emailTo,
+            reportLinks  : reportLinks
+        ], this)
+
+        throw e
     }
 }
